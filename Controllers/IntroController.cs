9181eff2503e101e5c8695e3fac02af94734aace
Tope-stack <yusufftope70@gmail.com>
2022-10-28 14:21:57 +0100

    Add project files.

---
+++ Controllers/IntroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hng_task_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IntroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Intro intro = new Intro();
+            intro.slackUsername = "Temitope_y";
+            intro.backend = true;
+            intro.age = 22;
+            intro.bio = "Aspiring Backend Developer";
+
+            
+            return Ok(intro);
+        }
+    }
+}
---
+++ Intro.cs
@@ -0,0 +1,10 @@
+﻿namespace hng_task_1
+{
+    public class Intro
+    {
+        public string slackUsername { get; set; } = "";
+        public bool backend { get; set; } = true;
+        public int age { get; set; } = 0;
+        public string bio { get; set; } = "";
+    }
+}
